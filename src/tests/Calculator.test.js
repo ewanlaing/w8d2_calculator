d47694e7d968e0114650957c1f902f9d21117a9f
ewanlaing <117400398+ewanlaing@users.noreply.github.com>
2023-07-04 12:47:52 +0100

    unit tests done

---
+++ src/tests/Calculator.test.js
@@ -0,0 +1,119 @@
+import React from 'react';
+import Calculator from '../containers/Calculator';
+import {render, fireEvent} from '@testing-library/react';
+
+describe('Calculator', () => {
+  let container;
+  beforeEach(() => {
+    container = render(<Calculator/>)
+  })
+
+  it('should change running total on number enter', () => {
+    const button4 = container.getByTestId('number4');
+    const runningTotal = container.getByTestId('running-total');
+    fireEvent.click(button4);
+    expect(runningTotal.textContent).toEqual('4');
+  })
+
+  it('should add numbers together', () => {
+    const button1 = container.getByTestId('number1');
+    const button4 = container.getByTestId('number4');
+    const add = container.getByTestId('operator-add');
+    const equals = container.getByTestId('operator-equals');
+    const runningTotal = container.getByTestId('running-total');
+    fireEvent.click(button1);
+    fireEvent.click(add);
+    fireEvent.click(button4);
+    fireEvent.click(equals);
+    expect(runningTotal.textContent).toEqual('5');
+  })
+
+  it('should be able to subtract numbers', () => {
+    const button4 = container.getByTestId('number4');
+    const button7 = container.getByTestId('number7');
+    const subtract = container.getByTestId('operator-subtract');
+    const equals = container.getByTestId('operator-equals');
+    const runningTotal = container.getByTestId('running-total');
+    fireEvent.click(button7);
+    fireEvent.click(subtract);
+    fireEvent.click(button4);
+    fireEvent.click(equals);
+    expect(runningTotal.textContent).toEqual('3');
+  })
+
+  it('should be able to multiply numbers', () => {
+    const button3 = container.getByTestId('number3');
+    const button5 = container.getByTestId('number5');
+    const multiply = container.getByTestId('operator-multiply');
+    const equals = container.getByTestId('operator-equals');
+    const runningTotal = container.getByTestId('running-total');
+    fireEvent.click(button3);
+    fireEvent.click(multiply);
+    fireEvent.click(button5);
+    fireEvent.click(equals);
+    expect(runningTotal.textContent).toEqual('15');
+  })
+
+  it('should be able to divide numbers', () => {
+    const button2 = container.getByTestId('number2');
+    const button1 = container.getByTestId('number1');
+    const button7 = container.getByTestId('number7');
+    const divide = container.getByTestId('operator-divide');
+    const equals = container.getByTestId('operator-equals');
+    const runningTotal = container.getByTestId('running-total');
+    fireEvent.click(button2);
+    fireEvent.click(button1);
+    fireEvent.click(divide);
+    fireEvent.click(button7);
+    fireEvent.click(equals);
+    expect(runningTotal.textContent).toEqual('3');
+  })
+
+  it('should be able to concatenate multiple number button clicks', () => {
+    const button1 = container.getByTestId('number1');
+    const button2 = container.getByTestId('number2');
+    const button3 = container.getByTestId('number3');
+    const runningTotal = container.getByTestId('running-total');
+    fireEvent.click(button1);
+    fireEvent.click(button2);
+    fireEvent.click(button3);
+    expect(runningTotal.textContent).toEqual('123');
+  })
+
+  it('should be able to chain multiple operations together', () => {
+    const button1 = container.getByTestId('number1');
+    const button2 = container.getByTestId('number2');
+    const button3 = container.getByTestId('number3');
+    const add = container.getByTestId('operator-add');
+    const equals = container.getByTestId('operator-equals');
+    const runningTotal = container.getByTestId('running-total');
+    fireEvent.click(button1);
+    fireEvent.click(add);
+    fireEvent.click(button2);
+    fireEvent.click(add);
+    fireEvent.click(button3);
+    fireEvent.click(equals);
+    expect(runningTotal.textContent).toEqual('6');
+  })
+
+  it('should be able to clear the current total without affecting the calculation', () => {
+    const button1 = container.getByTestId('number1');
+    const button2 = container.getByTestId('number2');
+    const button3 = container.getByTestId('number3');
+    const add = container.getByTestId('operator-add');
+    const equals = container.getByTestId('operator-equals');
+    const clear = container.getByTestId('clear');
+    const runningTotal = container.getByTestId('running-total');
+    fireEvent.click(button1);
+    fireEvent.click(add);
+    fireEvent.click(button2);
+    fireEvent.click(clear);
+    fireEvent.click(add);
+    fireEvent.click(button3);
+    fireEvent.click(equals);
+    expect(runningTotal.textContent).toEqual('4');
+  })
+
+
+})
+
